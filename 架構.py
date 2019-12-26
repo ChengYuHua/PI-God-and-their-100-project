a8b0dd900f7cf6854aeda6acf24341c4c7c0af61
Tsai-Yi-Hung <58772840+Tsai-Yi-Hung@users.noreply.github.com>
2019-12-27 02:21:28 +0800

    Update 架構.py

--- 架構.py
+++ 架構.py
@@ -12,21 +12,27 @@ CP值：cp_good、cp_bad
 class Restaraunt:
 # 第一個位置放累積正評數，第二個位置放累積中立數，第三個位置放累積負評數
 # articles是累積文章數
-
-	service = [0, 0, 0]
-	food = [0, 0, 0]
-	cp = [0, 0, 0]
-	environment = [0, 0, 0]
-	reachable = [0, 0, 0]
-	speed = [0, 0, 0]
-	total = [0, 0]  # 中立就跳過不要計
-	articles = 0 
+    service = [0, 0, 0]
+    food = [0, 0, 0]
+    cp = [0, 0, 0]
+    environment = [0, 0, 0]
+    reachable = [0, 0, 0]
+    speed = [0, 0, 0]
+    total = [0, 0]  # 中立就跳過不要計
+    articles = 0
+    ptt_url = []
+    dcard_url = []
+    ifoodie_url = []
+    pixnet_url = []
 	
-	# 函數呼叫時，依序回傳該細項正評總分、中立總分和負評總分
+    # 函數呼叫時，依序回傳該細項正評總分、中立總分和負評總分
 
-	def total_service(self):
-		return (self.service[0] / articles), (self.service[1] / articles) , (self.service[2] / articles)
-		# 這邊只列一個，其他略過
+    def total_service(self):
+	return (self.service[0] / articles), (self.service[1] / articles) , (self.service[2] / articles)
+    # 這邊只列一個，其他略過
+
+class Article:
+    # 這邊放計算單篇文章的分數和細項的函數
 
 讓使用者依序輸入地點和食物種類
 location = input()
@@ -34,7 +40,7 @@ category = input()
 建一個 all_restaraunt 的 list，之後會拿來存所有餐廳的評分和資訊
 
 2.程式動起來
-使用爬蟲找出所有符合條件的餐廳，放入一個 list（restaraunts_list）（滑）
+使用爬蟲找出所有符合條件的餐廳，放入一個 list(restaraunts_list)
 使用 for 迴圈從 restaraunts_list 裡，一家一家餐廳抓出來
 把餐廳變成 Restaurant class
 i = Restaurant()
