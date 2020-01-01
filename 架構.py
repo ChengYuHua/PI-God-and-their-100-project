7ac9529747a480ec95ce9f90dad85b0e3f1bbcf4
Tsai-Yi-Hung <58772840+Tsai-Yi-Hung@users.noreply.github.com>
2020-01-01 21:48:12 +0800

    Update 架構.py

--- 架構.py
+++ 架構.py
@@ -185,8 +185,8 @@ def dcard_crawler(dcard_soup):  # 餐廳的一個屬性
 
 url = 'https://ifoodie.tw/post/5dea600d2261390a2235125d-%E5%8F%B0%E5%8C%97%E8%90%AC%E8%8F%AF%E6%96%B0%E5%8C%97%E4%B8%89%E9%87%8D%E8%B6%85%E5%A5%BD%E5%90%83%E8%8A%B1%E7%94%9F%E6%B2%BE%E9%86%AC%E7%9F%B3%E9%A0%AD%E7%81%AB%E9%8D%8B%E6%8E%A8'
 results = requests.get(url)
-r.encoding='utf-8'
-soup = BeautifulSoup(r.text, 'html.parser')
+results.encoding='utf-8'
+soup = BeautifulSoup(results.text, 'html.parser')
 selector = etree.HTML(results.text)  # 只有ptt會用到
 
 def ifoodie_crawler():
