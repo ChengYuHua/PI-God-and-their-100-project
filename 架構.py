81fbc443feed23addf540b5cba25a967ee53f223
Tsai-Yi-Hung <58772840+Tsai-Yi-Hung@users.noreply.github.com>
2020-01-03 18:07:43 +0800

    Update 架構.py

--- 架構.py
+++ 架構.py
@@ -1,4 +1,5 @@
-mport requests
+import tkinter as tk
+import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
@@ -7,63 +8,6 @@ import re
 import numpy as np
 import jieba
 import csv
-import tkinter as tk
-from tkinter import ttk
-import tkinter.font as tkFont
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
-
-foodType = input()  # 輸入食物種類
-place = input()  # 輸入地點
-
-"""
-1.前置作業
-import 需要的 library
-用excel建置六面向和總評的精準、模糊語料庫來判斷正、負評
-讀入csv
-
-服務：service_good、service_bad
-餐點：food_good、food_bad
-CP值：cp_good、cp_bad
-環境：environment_good、environment_bad
-交通易達性：reachable_good、reachable_bad
-等待時間：speed_good、speed_bad
-
-建餐聽的 class，專門計算細項分數
-class Restaraunt:
-# 第一個位置放累積正評數，第二個位置放累積中立數，第三個位置放累積負評數
-# articles是累積文章數
-    service = [0, 0, 0]
-    food = [0, 0, 0]
-    cp = [0, 0, 0]
-    environment = [0, 0, 0]
-    reachable = [0, 0, 0]
-    speed = [0, 0, 0]
-    total = [0, 0]  # 中立就跳過不要計
-    articles = 0
-    ptt_url = []  # ppt網址
-    dcard_url = []  # dcard網址
-    ifoodie_url = []  # ifoodie網址
-    pixnet_url = []  # pixnet網址
-
-    # 函數呼叫時，依序回傳該細項正評總分、中立總分和負評總分
-
-    def total_service(self):
-        return (self.service[0] / articles), (self.service[1] / articles) , (self.service[2] / articles)
-    # 這邊只列一個，其他略過
-
-建文章的class，用來計算單篇文章的各項分數
-class Article:
-    # 這邊放計算單篇文章的分數和細項的函數
-    # aka語料庫比對的部分
-
-讓使用者依序輸入地點和食物種類
-location = input()
-category = input()
-建一個 all_score 的 list，之後會拿來存所有餐廳的評分和資訊
-
-2.程式動起來
-"""
 
 
 # clean all the punctuation and others 把文章斷詞的def（已經debug）
@@ -223,35 +167,6 @@ def rest_count(a_clean_text):
     return total_score, service_score, food_score, cp_score, environment_score, reachable_score, speed_score
 
 
-A = Restaurant()
-B = Restaurant()
-C = Restaurant()
-D = Restaurant()
-E = Restaurant()
-F = Restaurant()
-G = Restaurant()
-H = Restaurant()
-I = Restaurant()
-J = Restaurant()
-K = Restaurant()
-L = Restaurant()
-M = Restaurant()
-N = Restaurant()
-O = Restaurant()
-P = Restaurant()
-Q = Restaurant()
-R = Restaurant()
-S = Restaurant()
-T = Restaurant()
-U = Restaurant()
-V = Restaurant()
-W = Restaurant()
-X = Restaurant()
-Y = Restaurant()
-Z = Restaurant()
-restaurant_objects = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
-
-
 # 從google map爬餐廳會用到的函數：parser、restaurant_crawler
 def parser():
     html = driver.page_source
@@ -371,171 +286,284 @@ def pixnet_crawler(pixnet_soup):
         article += tag.get_text()
     return article
 
+"""
+A = Restaurant()
+B = Restaurant()
+C = Restaurant()
+D = Restaurant()
+E = Restaurant()
+F = Restaurant()
+G = Restaurant()
+H = Restaurant()
+I = Restaurant()
+J = Restaurant()
+K = Restaurant()
+L = Restaurant()
+M = Restaurant()
+N = Restaurant()
+O = Restaurant()
+P = Restaurant()
+Q = Restaurant()
+R = Restaurant()
+S = Restaurant()
+T = Restaurant()
+U = Restaurant()
+V = Restaurant()
+W = Restaurant()
+X = Restaurant()
+Y = Restaurant()
+Z = Restaurant()
+restaurant_objects = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
+"""
 
-restaurants_list = restaurant_crawler(foodType, place)  # 使用爬蟲從google map找出所有符合條件的餐廳，用list的形式存入restaurants 的 list
-for i in range(len(restaurants_list)):  # 使用 for 迴圈從 restaurants 的 list 裡，一家一家餐廳抓出來
-    if i <= 25:  # 可改參數
-        store_i = i
-        restaurant_objects[i].name = restaurants_list[i]  # restaurant_objects[i] 是 class
-        all_urls = []
-        total_add = np.array([0, 0, 0])
-        service_add = np.array([0, 0, 0])
-        food_add = np.array([0, 0, 0])
-        cp_add = np.array([0, 0, 0])
-        env_add = np.array([0, 0, 0])
-        reach_add = np.array([0, 0, 0])
-        speed_add = np.array([0, 0, 0])
-        for a_site in sites:
-            all_urls.append(google_crawler(restaurants_list[i], a_site))
-        for j in range(len(all_urls)):  # 輸入順序，選擇論壇
-            # 一家餐廳在各篇文章中，六面向的正負比例
+def find_rest(food_type, place):
+    A = Restaurant()
+    B = Restaurant()
+    C = Restaurant()
+    D = Restaurant()
+    E = Restaurant()
+    F = Restaurant()
+    G = Restaurant()
+    H = Restaurant()
+    I = Restaurant()
+    J = Restaurant()
+    K = Restaurant()
+    L = Restaurant()
+    M = Restaurant()
+    N = Restaurant()
+    O = Restaurant()
+    P = Restaurant()
+    Q = Restaurant()
+    R = Restaurant()
+    S = Restaurant()
+    T = Restaurant()
+    U = Restaurant()
+    V = Restaurant()
+    W = Restaurant()
+    X = Restaurant()
+    Y = Restaurant()
+    Z = Restaurant()
+    restaurant_objects = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
+    restaurants_list = restaurant_crawler(food_type, place)  # 使用爬蟲從google map找出所有符合條件的餐廳，用list的形式存入restaurants 的 list
+    for i in range(len(restaurants_list)):  # 使用 for 迴圈從 restaurants 的 list 裡，一家一家餐廳抓出來
+        if i <= 0:  # 可改參數
+            store_i = i
+            restaurant_objects[i].name = restaurants_list[i]  # restaurant_objects[i] 是 class
+            all_urls = []
+            total_add = np.array([0, 0, 0])
+            service_add = np.array([0, 0, 0])
+            food_add = np.array([0, 0, 0])
+            cp_add = np.array([0, 0, 0])
+            env_add = np.array([0, 0, 0])
+            reach_add = np.array([0, 0, 0])
+            speed_add = np.array([0, 0, 0])
+            for a_site in sites:
+                all_urls.append(google_crawler(restaurants_list[i], a_site))
+            for j in range(len(all_urls)):  # 輸入順序，選擇論壇
+                # 一家餐廳在各篇文章中，六面向的正負比例
 
-            for k in range(len(all_urls[j])):  # 輸入各個網址，爬出文章
-                results = requests.get(all_urls[j][k])
-                results.encoding = 'utf-8'
-                soup = BeautifulSoup(results.text, 'html.parser')
-                if j == 0:
-                    selector = etree.HTML(results.text)
-                    article = ptt_crawler(soup, selector, restaurants_list[i])
-                elif j == 1:
-                    article = dcard_crawler(soup, restaurants_list[i])
-                elif j == 2:
-                    article = ifoodie_crawler(soup)
-                elif j == 3:
-                    article = pixnet_crawler(soup)
-                else:
-                    continue
-                if type(article) == 'NoneType' or article is None:
-                    continue
-                else:
-                    clean_text = text_clean(article)
+                for k in range(len(all_urls[j])):  # 輸入各個網址，爬出文章
+                    results = requests.get(all_urls[j][k])
+                    results.encoding = 'utf-8'
+                    soup = BeautifulSoup(results.text, 'html.parser')
+                    if j == 0:
+                        selector = etree.HTML(results.text)
+                        article = ptt_crawler(soup, selector, restaurants_list[i])
+                    elif j == 1:
+                        article = dcard_crawler(soup, restaurants_list[i])
+                    elif j == 2:
+                        article = ifoodie_crawler(soup)
+                    elif j == 3:
+                        article = pixnet_crawler(soup)
+                    else:
+                        continue
+                    if type(article) == 'NoneType' or article is None:
+                        continue
+                    else:
+                        clean_text = text_clean(article)
 
-                article_total, article_service, article_food, article_cp, article_env, article_reach, article_speed = rest_count(
-                    clean_text)
-                # 單篇文章累加成一家餐廳
-                total_add += article_total
-                service_add += article_service
-                food_add += article_food
-                cp_add += article_cp
-                env_add += article_env
-                reach_add += article_reach
-                speed_add += article_speed
-        i = store_i
-        # 最後會輸出的是 一間餐廳的總文章正負比例
-        restaurant_objects[i].total = [int(total_add[0]) / int((total_add[0] + total_add[1] + total_add[2])), int(total_add[2]) / int((total_add[0] + total_add[1] + total_add[2]))]
-        restaurant_objects[i].service = [int(service_add[0]) / int((service_add[0] + service_add[1] + service_add[2])), int(service_add[2]) / int((service_add[0] + service_add[1] + service_add[2]))]
-        restaurant_objects[i].food = [int(food_add[0]) / int((food_add[0] + food_add[1] + food_add[2])), int(food_add[2]) / int((food_add[0] + food_add[1] + food_add[2]))]
-        restaurant_objects[i].cp = [int(cp_add[0]) / int((cp_add[0] + cp_add[1] + cp_add[2])), int(cp_add[2]) / int((cp_add[0] + cp_add[1] + cp_add[2]))]
-        restaurant_objects[i].speed = [int(speed_add[0]) / int((speed_add[0] + speed_add[1] + speed_add[2])), int(speed_add[2]) / int((speed_add[0] + speed_add[1] + speed_add[2]))]
-        restaurant_objects[i].env = [int(env_add[0]) / int((env_add[0] + env_add[1] + env_add[2])), int(env_add[2]) / int((env_add[0] + env_add[1] + env_add[2]))]
-        restaurant_objects[i].reach = [int(reach_add[0]) / int((reach_add[0] + reach_add[1] + reach_add[2])), int(reach_add[2]) / int((reach_add[0] + reach_add[1] + reach_add[2]))]
+                    article_total, article_service, article_food, article_cp, article_env, article_reach, article_speed = rest_count(
+                        clean_text)
+                    # 單篇文章累加成一家餐廳
+                    total_add += article_total
+                    service_add += article_service
+                    food_add += article_food
+                    cp_add += article_cp
+                    env_add += article_env
+                    reach_add += article_reach
+                    speed_add += article_speed
+            i = store_i
+            # 最後會輸出的是 一間餐廳的總文章正負比例
+            restaurant_objects[i].total = [int(total_add[0]) / int((total_add[0] + total_add[1] + total_add[2])),
+                                           int(total_add[2]) / int((total_add[0] + total_add[1] + total_add[2]))]
+            restaurant_objects[i].service = [
+                int(service_add[0]) / int((service_add[0] + service_add[1] + service_add[2])),
+                int(service_add[2]) / int((service_add[0] + service_add[1] + service_add[2]))]
+            restaurant_objects[i].food = [int(food_add[0]) / int((food_add[0] + food_add[1] + food_add[2])),
+                                          int(food_add[2]) / int((food_add[0] + food_add[1] + food_add[2]))]
+            restaurant_objects[i].cp = [int(cp_add[0]) / int((cp_add[0] + cp_add[1] + cp_add[2])),
+                                        int(cp_add[2]) / int((cp_add[0] + cp_add[1] + cp_add[2]))]
+            restaurant_objects[i].speed = [int(speed_add[0]) / int((speed_add[0] + speed_add[1] + speed_add[2])),
+                                           int(speed_add[2]) / int((speed_add[0] + speed_add[1] + speed_add[2]))]
+            restaurant_objects[i].env = [int(env_add[0]) / int((env_add[0] + env_add[1] + env_add[2])),
+                                         int(env_add[2]) / int((env_add[0] + env_add[1] + env_add[2]))]
+            restaurant_objects[i].reach = [int(reach_add[0]) / int((reach_add[0] + reach_add[1] + reach_add[2])),
+                                           int(reach_add[2]) / int((reach_add[0] + reach_add[1] + reach_add[2]))]
 
-    else:
-        break
+        else:
+            break
 
-# 排序
-# 下列各list會裝各家餐廳的各項總分
-all_total = []
-all_service = []
-all_food = []
-all_cp = []
-all_env = []
-all_reach = []
-all_speed = []
-for i in range(len(restaurant_objects)):
-    all_total.append(restaurant_objects[i].total[0])
-    all_service.append(restaurant_objects[i].service[0])
-    all_food.append(restaurant_objects[i].food[0])
-    all_cp.append(restaurant_objects[i].cp[0])
-    all_env.append(restaurant_objects[i].env[0])
-    all_reach.append(restaurant_objects[i].reach[0])
-    all_speed.append(restaurant_objects[i].speed[0])
+    # 排序
+    # 下列各list會裝各家餐廳的各項總分
+    all_total = []
+    all_service = []
+    all_food = []
+    all_cp = []
+    all_env = []
+    all_reach = []
+    all_speed = []
+    for i in range(len(restaurant_objects)):
+        all_total.append(restaurant_objects[i].total[0])
+        all_service.append(restaurant_objects[i].service[0])
+        all_food.append(restaurant_objects[i].food[0])
+        all_cp.append(restaurant_objects[i].cp[0])
+        all_env.append(restaurant_objects[i].env[0])
+        all_reach.append(restaurant_objects[i].reach[0])
+        all_speed.append(restaurant_objects[i].speed[0])
 
-# 下列各list會裝排序過後的餐廳總分，用index表示是第幾個object
-total_sort = []
-service_sort = []
-food_sort = []
-cp_sort = []
-env_sort = []
-reach_sort = []
-speed_sort = []
+    # 下列各list會裝排序過後的餐廳總分，用index表示是第幾個object
+    total_sort = []
+    service_sort = []
+    food_sort = []
+    cp_sort = []
+    env_sort = []
+    reach_sort = []
+    speed_sort = []
 
-for i in range(len(all_total)):  # 每跑一個迴圈可以找到未被選取過的數值中的最大值
-    max_total = all_total[i]
-    max_service = all_service[i]
-    max_food = all_food[i]
-    max_cp = all_cp[i]
-    max_env = all_env[i]
-    max_reach = all_reach[i]
-    max_speed = all_speed[i]
-    total_index = i
-    service_index = i
-    food_index = i
-    cp_index = i
-    env_index = i
-    reach_index = i
-    speed_index = i
+    for i in range(len(all_total)):  # 每跑一個迴圈可以找到未被選取過的數值中的最大值
+        max_total = all_total[i]
+        max_service = all_service[i]
+        max_food = all_food[i]
+        max_cp = all_cp[i]
+        max_env = all_env[i]
+        max_reach = all_reach[i]
+        max_speed = all_speed[i]
+        total_index = i
+        service_index = i
+        food_index = i
+        cp_index = i
+        env_index = i
+        reach_index = i
+        speed_index = i
 
-    for j in range(len(all_total)):  # 把每一個數字和現有的最大值比較，找出最大的數值
-        if all_total[j] > max_total:
-            max_total = all_total[j]
-            total_index = j
-        if all_service[j] > max_service:
-            max_service = all_service[j]
-            service_index = j
-        if all_food[j] > max_food:
-            max_food = all_food[j]
-            food_index = j
-        if all_cp[j] > max_cp:
-            max_cp = all_cp[j]
-            cp_index = j
-        if all_env[j] > max_env:
-            max_env = all_env[j]
-            env_index = j
-        if all_reach[j] > max_reach:
-            max_reach = all_reach[j]
-            reach_index = j
-        if all_speed[j] > max_speed:
-            max_speed = all_speed[j]
-            speed_index = j
-    # 找到最大值後，存取index，並把最大值的位置改寫為０
-    total_sort.append(total_index)
-    service_sort.append(service_index)
-    food_sort.append(food_index)
-    cp_sort.append(cp_index)
-    env_sort.append(env_index)
-    reach_sort.append(reach_index)
-    speed_sort.append(speed_index)
+        for j in range(len(all_total)):  # 把每一個數字和現有的最大值比較，找出最大的數值
+            if all_total[j] > max_total:
+                max_total = all_total[j]
+                total_index = j
+            if all_service[j] > max_service:
+                max_service = all_service[j]
+                service_index = j
+            if all_food[j] > max_food:
+                max_food = all_food[j]
+                food_index = j
+            if all_cp[j] > max_cp:
+                max_cp = all_cp[j]
+                cp_index = j
+            if all_env[j] > max_env:
+                max_env = all_env[j]
+                env_index = j
+            if all_reach[j] > max_reach:
+                max_reach = all_reach[j]
+                reach_index = j
+            if all_speed[j] > max_speed:
+                max_speed = all_speed[j]
+                speed_index = j
+        # 找到最大值後，存取index，並把最大值的位置改寫為０
+        total_sort.append(total_index)
+        service_sort.append(service_index)
+        food_sort.append(food_index)
+        cp_sort.append(cp_index)
+        env_sort.append(env_index)
+        reach_sort.append(reach_index)
+        speed_sort.append(speed_index)
 
-    all_total[total_index] = 0
-    all_service[service_index] = 0
-    all_food[food_index] = 0
-    all_cp[cp_index] = 0
-    all_env[env_index] = 0
-    all_reach[reach_index] = 0
-    all_speed[speed_index] = 0
+        all_total[total_index] = 0
+        all_service[service_index] = 0
+        all_food[food_index] = 0
+        all_cp[cp_index] = 0
+        all_env[env_index] = 0
+        all_reach[reach_index] = 0
+        all_speed[speed_index] = 0
+    return restaurant_objects, total_sort, service_sort, food_sort, cp_sort, env_sort, reach_sort, speed_sort
 
-print(restaurant_objects[total_sort[0]].total[0])
-print(restaurant_objects[total_sort[0]].name)
 
-print(restaurant_objects[total_sort[1]].total[0])
-print(restaurant_objects[total_sort[1]].name)
+x = str()
+y = str()
+aa = []
+bb = []
+cc = []
+dd = []
+ee = []
+ff = []
+gg = []
+hh = []
 
-print(restaurant_objects[total_sort[2]].total[0])
-print(restaurant_objects[total_sort[2]].name)
+window = tk.Tk()
+# 設定視窗標題、大小和背景顏色
+window.title('食神')
+window.geometry('800x600')
+window.configure(background='white')
+
+
+def calculate_food():
+    global x, y, aa, bb, cc, dd, ee, ff, gg, hh
+    x = str(height_entry.get())
+    y = str(weight_entry.get())
+    aa, bb, cc, dd, ee, ff, gg, hh = find_rest(x, y)
+    apple = aa[bb[0]].total[0]
+    pineapple = aa[bb[0]].name
+    result = 'No.1 推薦餐廳：{} {}'.format(pineapple, apple)
+    result_label.configure(text=result)
 
 """
-2.用爬蟲找出該餐廳的文章網址，用list的形式存入該餐廳的Restaurant class中的ppt_url、dcard_url、ifoodie_url、pixnet_url
+    orange = aa[bb[1]].total[0]
+    orange = str(round(float(orange), 2) * 100)
+    orange += "%"
+    banana = aa[bb[1]].name
 
-    再跑一個迴圈 run 過所有該餐廳的評論文章(會以字串形式傳入)
-    用Article class計算文章的分數
-    計算玩分數後，紀錄於Restaurant class中
-    若一篇文正負評都沒有，就跳過。有的話就Restaraunt.articles += 1
+    tomato = aa[bb[2]].total[0]
+    tomato = str(round(float(tomato), 2) * 100)
+    tomato += "%"
+    pig = aa[bb[0]].name
 
-    餐廳的所有文章 run 完後，將餐廳名稱、各項分數編入 a_list，為該餐廳的 list
-    將 a_list 放入 all_score 的 list
-    持續 run 到所以餐廳都跑過為止    
+    result = 'No.1 推薦餐廳：{} {}\nNo.2 推薦餐廳：{} {}\nNo.3 推薦餐廳：{} {}'.format(pineapple, apple, orange, banana, tomato, pig)"""
 
-3.輸出
-視使用者要什麼（輸入什麼）從 all_restaraunt 選取該項目的 index，進行比較，由高到低輸出
-"""
+
+header_label = tk.Label(window, text='美食推薦系統')
+header_label.pack()
+
+# 以下為 height_frame 群組
+height_frame = tk.Frame(window)
+# 向上對齊父元件
+height_frame.pack(side=tk.TOP)
+height_label = tk.Label(height_frame, text='食物種類')
+height_label.pack(side=tk.LEFT)
+height_entry = tk.Entry(height_frame)
+height_entry.pack(side=tk.LEFT)
+
+# 以下為 weight_frame 群組
+weight_frame = tk.Frame(window)
+weight_frame.pack(side=tk.TOP)
+weight_label = tk.Label(weight_frame, text='地點')
+weight_label.pack(side=tk.LEFT)
+weight_entry = tk.Entry(weight_frame)
+weight_entry.pack(side=tk.LEFT)
+
+result_label = tk.Label(window)
+result_label.pack()
+
+calculate_btn = tk.Button(window, text='GO', command=calculate_food)
+calculate_btn.pack()
+
+# 運行主程式
+window.mainloop()
